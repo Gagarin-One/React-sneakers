0c180f2c75472cc67a3926555d82cc3a527cdcee
Gagarin-One <92833239+Gagarin-One@users.noreply.github.com>
2023-01-26 21:05:52 +0900

    top header

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -4,7 +4,21 @@ import React from "react"
 const Header = () => {
   return (
     <div>
-      
+      <div className="top-header">
+        <div className="login">
+          <h2>Вход</h2>
+          <h2 className="red">или</h2>
+          <h2>регистрация</h2>
+        </div>
+        <h2 className="delivery">Оплата и доставка</h2>
+        <div className="contacts">
+          <h2 className="schedule">8-22 будни · 8-20 выходные</h2>
+          <h2>+7 800 700 32 53</h2>
+        </div>
+      </div>
+      <div className="bottom-header">
+        
+      </div>
     </div>
   )
 }
