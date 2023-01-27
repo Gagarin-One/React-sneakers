76957f6cef84c7b517c4a64ca316117c3e0e5956
Gagarin-One <92833239+Gagarin-One@users.noreply.github.com>
2023-01-27 22:38:51 +0900

    main page

--- src/Layouts/MainLayout.tsx
+++ src/Layouts/MainLayout.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom"
+import Footer from "../Components/Footer";
 import Header from "../Components/Header"
 
 const MainLayout = () => {
   return (
-    <div>
-      <div className="wrapper">
+    <div className="wrapper">
+      <div className="container">
         <Header />
         <div>
           <Outlet/>
         </div>
+        <Footer/>
       </div>
     </div>
   );
