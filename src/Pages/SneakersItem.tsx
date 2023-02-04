a18236418050e6fa0e047dabf0c1d2dd6692638d
Gagarin-One <92833239+Gagarin-One@users.noreply.github.com>
2023-02-04 19:54:02 +0900

    full sneakers page

--- src/Pages/SneakersItem.tsx
+++ src/Pages/SneakersItem.tsx
@@ -1,18 +1,52 @@
-import { FC, useEffect } from 'react';
-import { useLocation } from 'react-router';
-import { useGetNikesQuery } from '../Store/Api/Api';
-
+import { useLocation, useParams } from 'react-router';
+import { Link } from 'react-router-dom';
+import { useAddToCartMutation, useGetNikesQuery } from '../Store/Api/Api';
+type Products = {
+  id: number;
+  price: number;
+  name: string;
+  size: number;
+  img: string;
+  desc: string;
+};
+const defaultProducts = {
+  id: 0,
+  price: 23,
+  name: 'dsd',
+  size: 44,
+  img: 'sdsd',
+  desc: 'sdsdsd',
+};
 const SneakersItem = () => {
-  const Location = useLocation()
-  const { data = [] } = useGetNikesQuery(Number(Location.pathname.slice(-1)));
+  const Location = useLocation();
+  const LocationId = Number(Location.pathname.slice(-1));
+  const { data = defaultProducts } = useGetNikesQuery(LocationId);
+  const [addToCart] = useAddToCartMutation();
 
- console.log(Location)
+  const handleAddToCart = async (item: Products) => {
+    await addToCart(item).unwrap();
+  };
+
   return (
-    <div>
-      <img src="" />
-      <div>4545</div>
-      <div>
-        <h1>{data.map((item) =>{return <div>{item.id}</div>})}</h1>
+    <div className="sneakers-wrapper">
+      <div className="sneaker">
+        <img src={data.img} />
+        <div className="side-block">
+          <h1>{data.name}</h1>
+          <b>{data.price}</b>
+          <button onClick={() => handleAddToCart({ ...data, ...{ size: 10 } })}>
+            Добавить в корзину
+          </button>
+          <span>Товар доступен только по полной предоплате</span>
+          <h2 className="title">Описание</h2>
+          <h2>{data.desc}</h2>
+          <Link to="Contacts" className="title">
+            Контакты
+          </Link>
+          <Link to="delivery" className="title">
+            Доставка и оплата
+          </Link>
+        </div>
       </div>
     </div>
   );
